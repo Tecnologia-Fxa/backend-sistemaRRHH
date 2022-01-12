b669e8c9b1c9a9ced220534ee24e6eaacabc7c77
FSCB21 <freddystibencb@gmail.com>
2022-01-12 12:47:17 -0500

    Relaciones tablas rrhh

--- Database/relacionesBD.js
+++ Database/relacionesBD.js
@@ -7,6 +7,27 @@ const TipoidentificacionModel = require('./Models/TipoIdentificacionModel');
 const TipoUsuarioModel = require('./Models/TipoUsuarioModel');
 const CentroCostoModel = require('./Models/CentroCostoModel');
 const TallaCalzadoModel = require('./Models/TallaCalzadoModel');
+const TallaPantalonModel = require('./Models/TallaPantalonModel');
+const EstadoCivilModel = require('./Models/EstadoCivilModel');
+const CargoModel = require('./Models/CargoModel');
+const CajaCompensacionModel = require('./Models/CajaCompensacionModel');
+const CesantiasModel = require('./Models/CesantiasModel');
+const PensionModel = require('./Models/PensionModel');
+const ArlModel = require('./Models/ArlModel');
+const BancoModel = require('./Models/BancoModel');
+const TipoCuentaModel = require('./Models/TipoCuentaModel');
+const EpsModel = require('./Models/EpsModel');
+const EstudiosRealizadosModel = require('./Models/EstudiosRealizadosModel');
+const TipoContratoModel = require('./Models/TipoContratoModel');
+const TallaCamisaModel = require('./Models/TallaCamisaModel');
+const NacionalidadModel = require('./Models/NacionalidadModel');
+const TiempoModel = require('./Models/TiempoModel');
+const EstadoContratoModel = require('./Models/EstadoContratoModel');
+const SalarioModel = require('./Models/SalarioModel');
+const EmpresaModel = require('./Models/EmpresaModel');
+const AuxMovilidadModel = require('./Models/AuxMovilidadModel');
+const DocumentosFaltantesModel = require('./Models/DocumentosFaltantesModel');
+const EmpleadoDocumentosFaltantesModel = require('./Models/EmpleadoDocumentosFaltantesModel');
 
 //Relaciones 1 a 1
 
@@ -32,6 +53,86 @@ EmpleadoModel.belongsTo(CentroCostoModel, { foreignKey:'centro_costo_fk' })
 TallaCalzadoModel.hasMany(EmpleadoModel, { as:'empleado', foreignKey:'talla_calzado_fk' });
 EmpleadoModel.belongsTo(TallaCalzadoModel, { foreignKey:'talla_calzado_fk' })
 
+//Relacion talla_pantalon y empleado (foranea en empleado)
+TallaPantalonModel.hasMany(EmpleadoModel, { as:'empleado', foreignKey:'talla_pantalon_fk' });
+EmpleadoModel.belongsTo(TallaPantalonModel, { foreignKey:'talla_pantalon_fk' })
+
+//Relacion talla_camisa y empleado (foranea en empleado)
+TallaCamisaModel.hasMany(EmpleadoModel, { as:'empleado', foreignKey:'talla_camisa_fk' });
+EmpleadoModel.belongsTo(TallaCamisaModel, { foreignKey:'talla_camisa_fk' })
+
+//Relacion estado_civil y empleado (foranea en empleado)
+EstadoCivilModel.hasMany(EmpleadoModel, { as:'empleado', foreignKey:'estado_civil_fk' });
+EmpleadoModel.belongsTo(EstadoCivilModel, { foreignKey:'estado_civil_fk' })
+
+//Relacion nacionalidad y empleado (foranea en empleado)
+NacionalidadModel.hasMany(EmpleadoModel, { as:'empleado', foreignKey:'nacionalidad_fk' });
+EmpleadoModel.belongsTo(NacionalidadModel, { foreignKey:'nacionalidad_fk' })
+
+//Relacion cargo y empleado (foranea en empleado)
+CargoModel.hasMany(EmpleadoModel, { as:'empleado', foreignKey:'cargo_fk' });
+EmpleadoModel.belongsTo(CargoModel, { foreignKey:'cargo_fk' })
+
+//Relacion caja_compensacion y empleado (foranea en empleado)
+CajaCompensacionModel.hasMany(EmpleadoModel, { as:'empleado', foreignKey:'ccf_fk' });
+EmpleadoModel.belongsTo(CajaCompensacionModel, { foreignKey:'ccf_fk' })
+
+//Relacion cesantias y empleado (foranea en empleado)
+CesantiasModel.hasMany(EmpleadoModel, { as:'empleado', foreignKey:'cesantias_fk' });
+EmpleadoModel.belongsTo(CesantiasModel, { foreignKey:'cesantias_fk' })
+
+//Relacion pension y empleado (foranea en empleado)
+PensionModel.hasMany(EmpleadoModel, { as:'empleado', foreignKey:'pension_fk' });
+EmpleadoModel.belongsTo(PensionModel, { foreignKey:'pension_fk' })
+
+//Relacion arl y empleado (foranea en empleado)
+ArlModel.hasMany(EmpleadoModel, { as:'empleado', foreignKey:'arl_fk' });
+EmpleadoModel.belongsTo(ArlModel, { foreignKey:'arl_fk' })
+
+//Relacion eps y empleado (foranea en empleado)
+EpsModel.hasMany(EmpleadoModel, { as:'empleado', foreignKey:'eps_fk' });
+EmpleadoModel.belongsTo(EpsModel, { foreignKey:'eps_fk' })
+
+//Relacion eps y empleado (foranea en empleado)
+BancoModel.hasMany(EmpleadoModel, { as:'empleado', foreignKey:'banco_fk' });
+EmpleadoModel.belongsTo(BancoModel, { foreignKey:'banco_fk' })
+
+//Relacion tipo_cuenta y empleado (foranea en empleado)
+TipoCuentaModel.hasMany(EmpleadoModel, { as:'empleado', foreignKey:'tipo_cuenta_fk' });
+EmpleadoModel.belongsTo(TipoCuentaModel, { foreignKey:'tipo_cuenta_fk' })
+
+//Relacion estudios_realizados y empleado (foranea en empleado)
+EstudiosRealizadosModel.hasMany(EmpleadoModel, { as:'empleado', foreignKey:'estudios_fk' });
+EmpleadoModel.belongsTo(EstudiosRealizadosModel, { foreignKey:'estudios_fk' })
+
+//Relacion tipo_contrato y empleado (foranea en empleado)
+TipoContratoModel.hasMany(EmpleadoModel, { as:'empleado', foreignKey:'tipo_contrato_fk' });
+EmpleadoModel.belongsTo(TipoContratoModel, { foreignKey:'tipo_contrato_fk' })
+
+//Relacion tiempo y empleado (foranea en empleado)
+TiempoModel.hasMany(EmpleadoModel, { as:'empleado', foreignKey:'tipo_tiempo_fk' });
+EmpleadoModel.belongsTo(TiempoModel, { foreignKey:'tipo_tiempo_fk' })
+
+//Relacion estado_contrato y empleado (foranea en empleado)
+EstadoContratoModel.hasMany(EmpleadoModel, { as:'empleado', foreignKey:'estado_contrato_fk' });
+EmpleadoModel.belongsTo(EstadoContratoModel, { foreignKey:'estado_contrato_fk' })
+
+//Relacion salario y empleado (foranea en empleado)
+SalarioModel.hasMany(EmpleadoModel, { as:'empleado', foreignKey:'salario_fk' });
+EmpleadoModel.belongsTo(SalarioModel, { foreignKey:'salario_fk' })
+
+//Relacion aux_movilidad y empleado (foranea en empleado)
+AuxMovilidadModel.hasMany(EmpleadoModel, { as:'empleado', foreignKey:'aux_movilidad_fk' });
+EmpleadoModel.belongsTo(AuxMovilidadModel, { foreignKey:'aux_movilidad_fk' })
+
+//Relacion empresa y empleado (foranea en empleado)
+EmpresaModel.hasMany(EmpleadoModel, { as:'empleado', foreignKey:'id_empresa_fk' });
+EmpleadoModel.belongsTo(EmpresaModel, { foreignKey:'id_empresa_fk' })
+
+//Relacion empleado y jefe_zona (foranea en empleado)
+EmpleadoModel.hasMany(EmpleadoModel, { as:'empleado', foreignKey:'jefe_zona_fk' });
+EmpleadoModel.belongsTo(EmpleadoModel, { foreignKey:'jefe_zona_fk' })
+
 //Relacion ciudad y empleado (foranea en empleado) => lugar Nacimiento
 CiudadModel.hasMany(EmpleadoModel, { as:'empleado_lugar_nacimiento', foreignKey:'lugar_nacimiento_fk' });
 EmpleadoModel.belongsTo(CiudadModel, { foreignKey:'lugar_nacimiento_fk' })
@@ -55,3 +156,11 @@ DocumentoModel.belongsTo(EmpleadoModel, { foreignKey:'empleado_fk' })
 //Relacion ciudad y centro_costo (foranea en centro costo)
 CiudadModel.hasMany(CentroCostoModel, { as:'centro_costo', foreignKey:'id_ciudad_fk' });
 CentroCostoModel.belongsTo(CiudadModel, { foreignKey:'id_ciudad_fk' })
+
+//Relacion documentos_faltantes y empleado_documentosfaltantes (foranea en empleado_documentosfaltantes)
+DocumentosFaltantesModel.hasMany(EmpleadoDocumentosFaltantesModel, { as:'empleado_documentosfaltantes', foreignKey:'id_documentos_faltantes_fk' });
+EmpleadoDocumentosFaltantesModel.belongsTo(DocumentosFaltantesModel, { foreignKey:'id_documentos_faltantes_fk' })
+
+//Relacion documentos_faltantes y empleado_documentosfaltantes (foranea en empleado_documentosfaltantes)
+EmpleadoModel.hasMany(EmpleadoDocumentosFaltantesModel, { as:'empleado_documentosfaltantes', foreignKey:'id_empleado_fk' });
+EmpleadoDocumentosFaltantesModel.belongsTo(EmpleadoModel, { foreignKey:'id_empleado_fk' })
