886168d0e762a832f68e4ced686fd08841ff6530
Freddy Calderon <freddystibencb@gmail.com>
2023-03-02 16:03:42 -0500

    Metodo para tawkto agregado

--- controllers/EmpleadoController.js
+++ controllers/EmpleadoController.js
@@ -564,7 +564,7 @@ const Controller = {
                 { model: CargoModel, attributes:['nombre_cargo']} 
             ]
         })
-        res.json({item,mensaje:"todo bien :D"})
+        res.json(item)
     }
 
 
