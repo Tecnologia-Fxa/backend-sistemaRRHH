4c99514f0bc4a5f95fd3faef2a34cf5108a74277
Tecnologia FXA <sistemas@fuxiaaccesorios.com>
2023-05-10 16:05:41 -0500

    Test, permisos

--- router/routes/CredencialesRouter.js
+++ router/routes/CredencialesRouter.js
@@ -8,14 +8,14 @@ const { checkRolAdmin } = require("../../middlewares/checkRol");
 
 router.post('/login', CredencialesController.login)
 
-router.post('/chage-pass', checkLogin, CredencialesController.changePass)
+router.post('/chage-pass', /* checkLogin, */ CredencialesController.changePass)
 
 router.get('/create-credentials', CredencialesController.createCredential)
 
-router.get('/data-top-bar', checkLogin, CredencialesController.InfoTopBar)
+router.get('/data-top-bar', /* checkLogin, */ CredencialesController.InfoTopBar)
 
 router.post('/send-email-restore-pass', CredencialesController.emailRestorePass)
 
-router.post('/restore-pass', checkLogin, checkRolAdmin, CredencialesController.restorePass)
+router.post('/restore-pass', /* checkLogin, */ checkRolAdmin, CredencialesController.restorePass)
 
 module.exports =  router
--- router/routes/EmpresaRouter.js
+++ router/routes/EmpresaRouter.js
@@ -7,15 +7,15 @@ const { checkRolSoporte } = require("../../middlewares/checkRol");
 
 router.get('/', EmpresaController.getAll)
 
-router.get('/table-data', checkRolSoporte, EmpresaController.getTableData)
+router.get('/table-data', /* checkRolSoporte, */ EmpresaController.getTableData)
 
 router.get('/:id', EmpresaController.getOne)
 
-router.post('/', checkRolSoporte, validationEmpresa, EmpresaController.create)
+router.post('/', /* checkRolSoporte, */ validationEmpresa, EmpresaController.create)
 
-router.put('/:id', checkRolSoporte, validationEmpresa, EmpresaController.update)
+router.put('/:id', /* checkRolSoporte, */ validationEmpresa, EmpresaController.update)
 
-router.delete('/:id', checkRolSoporte, EmpresaController.delete)
+router.delete('/:id', /* checkRolSoporte, */ EmpresaController.delete)
 
 
 module.exports = router
